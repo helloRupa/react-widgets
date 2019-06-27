0d5136410cd29fb849566688eaf237a1e81fa0bc
Rupa Dhillon <rupa211984@gmail.com>
2019-06-27 00:13:19 -0700

    Get weather

--- frontend/root.jsx
+++ frontend/root.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Clock from './clock';
 import Tabs from './tabs';
+import Weather from './weather';
 
 const Root = () => (
   <div>
     <Clock />
+    <Weather />
     <Tabs content={[
       {title: 'one', content: 'I am first'}, 
       {title: 'two', content: 'I am second'}, 
---
+++ frontend/weather.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import weatherResponse from '../dummy_data/weather';
+import keys from '../constants';
+
+class Weather extends React.Component {
+  constructor(params) {
+    super(params);
+    this.state = { loc: '', temp: '' };
+    this.fetchWeather = this.fetchWeather.bind(this);
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.fetchWeather(parseInt(position.coords.latitude, 10), parseInt(position.coords.longitude, 10));
+    });
+  }
+
+  fetchWeather(lat, long) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&APPID=`;
+
+    fetch(url + keys.weather)
+      .then((res) => {
+        if (res.status !== 200) {
+          console.log(`There was a problem. Status Code: ${res.status}`);
+          return;
+        }
+  
+        return res.json();
+      })
+      .then((res) => {
+        this.setState({ loc: res.name, temp: res.main.temp });
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({ loc: weatherResponse.name, temp: weatherResponse.main.temp });
+      });
+  }
+
+  render() {
+    return (
+      <div className="weather">
+        <h1>Weather</h1>
+        <div>
+          <p>{this.state.loc}</p>
+          <p>{this.state.temp}° F</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Weather;
